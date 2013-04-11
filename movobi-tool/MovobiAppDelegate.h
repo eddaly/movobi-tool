c23cb24522a37f0d1252610b6f75c2afc1647256
Ed Daly <ed@Eds-MacBook-Air.local>
2013-04-11 15:29:10 +0100

    Initial Commit

---
+++ movobi-tool/MovobiAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  MovobiAppDelegate.h
+//  movobi-tool
+//
+//  Created by Ed Daly on 11/04/2013.
+//  Copyright (c) 2013 Movobi Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MovobiAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ movobi-tool/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  movobi-tool
+//
+//  Created by Ed Daly on 11/04/2013.
+//  Copyright (c) 2013 Movobi Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
